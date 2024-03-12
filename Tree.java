914ffe73d0bf3756d58adbe232d54a305e253206
jesseguitar1987 <163082276+jesseguitar1987@users.noreply.github.com>
2024-03-11 22:31:11 -0400

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,11 @@
+public class Tree {
+  public static void main(String[] args) {
+    //This program plants a tree
+    System.out.println("Hello! I'm going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+  }
+}
